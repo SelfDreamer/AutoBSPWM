98866460ad8d3b981c0df9aa69090531ea5f2c4d
Flicking <152665972+FlickGMD@users.noreply.github.com>
2025-02-17 23:49:29 +0000

    Update nvim_upload.sh

--- nvim_upload.sh
+++ nvim_upload.sh
@@ -3,6 +3,3 @@ nvim_file=$(curl -s https://api.github.com/repos/neovim/neovim/releases/latest |
 [[ -d "/opt/nvim" ]] && sudo rm -rf /opt/nvim
 sudo mkdir -p /opt/nvim
 wget $nvim_file && sudo mv nvim*.tar.gz /opt/nvim && sudo 7z x /opt/nvim/nvim*.gz -o/opt/nvim && sudo tar -xf /opt/nvim/nvim*.tar -C /opt/nvim && sudo rm -rf /opt/nvim/nvim-linux-x86_64.tar && sudo rm -rf /opt/nvim/nvim-linux-x86_64.tar.gz 
-cat ~/.zshrc | sed "s|/opt/nvim.*bin|$(realpath /opt/nvim/nvim*64/bin)|" | sponge ~/.zshrc
-sudo cat ~/.zshrc | sed "s|/opt/nvim.*bin|$(realpath /opt/nvim/nvim*64/bin)|" | sponge ~/.zshrc
-

acafc0240a33c59e86b991d966e9a435b660a770
Flicking <152665972+FlickGMD@users.noreply.github.com>
2025-02-25 16:50:01 -0500

    Update InstallUserServersNvim.sh

--- InstallUserServersNvim.sh
+++ InstallUserServersNvim.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 servers=("bash-language-server" "lua-language-server" "basedpyright" "clangd" "rust-analyzer" "marksman")
-for server in "${servers[@]}"; do
-  nvim --headless -c "MasonInstall $server" +q # nvim --headless -c 'MasonInstall lua-language-server' +q & disown
-done
+if [[ $UID -eq 0 ]]; then
+  export NVIM=$(find /opt/nvim/nvim*/bin/ -iname nvim -type f -executable | head -n 1)
+    for server in "${servers[@]}"; do
+      $NVIM --headless -c "MasonInstall $server" +q # nvim --headless -c 'MasonInstall lua-language-server' +q & disown
+    done
+fi
+
+if [[ ! $UID -eq 0 ]]; then
+  for server in "${servers[@]}"; do
+    nvim --headless -c "MasonInstall $server" +q # nvim --headless -c 'MasonInstall lua-language-server' +q & disown
+  done
+fi

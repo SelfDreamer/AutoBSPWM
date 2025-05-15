83e069f214995e33400c6e0a22947c7d7daa54eb
Flicking <152665972+FlickGMD@users.noreply.github.com>
2025-05-15 16:39:04 -0500

    Update results.sh

--- config/bspwm/scripts/results.sh
+++ config/bspwm/scripts/results.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-packages=$(/bin/cat /tmp/data.txt 2>/dev/null | grep -oP '\d+(?= packages)' 2>/dev/null)
+packages=$(/bin/cat /tmp/data.txt 2>/dev/null | grep -oP '\d+(?= packages | paquetes)' 2>/dev/null)
 
 if [[ -z $packages ]]; then
   packages=0
 fi 
 
-echo $packages
+echo "$packages"

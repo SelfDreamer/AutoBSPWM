19c3c6d07cb7ae4f571ecb8b0f6722b19cbc470b
dreamer <dreamer@localhost.localdomain>
2026-01-27 20:44:08 -0500

    Changues

--- font.sh
+++ font.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 1. Definir ruta y cargar colores AL PRINCIPIO
 readonly path="$(realpath "$0" | rev | cut -d'/' -f2- | rev)"
 cd "${path}" || exit 1
 [[ -f ./Colors ]] && source ./Colors
@@ -12,21 +11,18 @@ function ctrl_c(){
 
 trap ctrl_c INT
 
-# 2. Definir VALORES POR DEFECTO (fuera de funciones para que sean accesibles)
-INPUT_IMAGE="./wallpapers/Themes/Latte/HTB.jpg"
+INPUT_IMAGE="./wallpapers/Themes/Default/HTB.jpg"
 FONT_PATH="/usr/share/fonts/truetype/HackNerdFont-Regular.ttf"
-OUTPUT_IMAGE="./wallpapers/Wallpaper.jpg"
+OUTPUT_IMAGE="./wallpapers/Themes/Default/wallpaper.jpg"
 FILL="white"
-NICKNAME="${NICKNAME}" # Vacío por defecto para detectar si se pasó por argumento o no
+NICKNAME="${NICKNAME}" 
 
-# 3. Bucle de argumentos CORREGIDO
-# Usamos $# -gt 0 (mientras el número de argumentos sea mayor a 0)
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
     --input-image)
       INPUT_IMAGE="$2"
-      shift 2 # Desplaza 2 posiciones (el flag y el valor)
+      shift 2 
       ;;
     --font-path)
       FONT_PATH="$2"
@@ -45,24 +41,20 @@ while [[ $# -gt 0 ]]; do
       shift 2
       ;;
     *)
-      # Captura argumentos desconocidos para evitar bucles infinitos o errores
       echo "Opción desconocida: $1"
       exit 1
       ;;
   esac
 done
 
-# 4. Lógica interactiva (Si no se pasó nickname por argumento)
 if [[ -z "${NICKNAME}" ]]; then
     echo -ne "\r${bright_cyan:-}[+]${bright_white:-} Introduce el nick que se vera reflejado en el fondo de pantalla: "
     read -r NICK_INPUT
     NICKNAME="${NICK_INPUT}"
 fi
 
-# Formatear el nick final (Si está vacío usa el usuario del sistema)
 FINAL_NICK="@${NICKNAME:-$USER}"
 
-# 5. Generar la imagen (Ya no necesita estar en una función aparte si es lineal)
 convert "$INPUT_IMAGE" \
       -font "$FONT_PATH" \
       -pointsize 48 \
